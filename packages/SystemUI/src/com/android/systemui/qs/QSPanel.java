098a54df262578148fdd6c28c096384839d79194
maxwen <max.weninger@gmail.com>
2022-04-27 17:33:18 +0800

    SystemUI: QS customizations settings [1/2]
    includes:
    
    [maxwenn & cjh1249131356]
    * SystemUI: hide label and vertical layout settings
    * base: QS tiles layout customizations
    
    - Inspired from https://github.com/omnirom/android_frameworks_base/commit/4d71992f4446d828e0f5e7eb3841632ea28ee71c.
      To avoid breaking aosp performance of calculating stuff here and there, use overlay to implement this.
    
    [minaripenguin]
    * SystemUI: Fix qs tile vertical layout scrolling
    * SystemUI: Simpflify updateLayout and debrand QSLayoutUtils/overlays to com.android
    * Re-evaluate hide label and vertical layout settings on settings change
    * SystemUI: Use DelayableMarqueeTextView util for vertical qs tile as well
    * Adapted to A15
    * move overlays to vendor/addons
    
    [timjosten]
    * SystemUI: Improve vertical QS tile layout & animation
    
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: timjosten <timjosten@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Ib6dcddd8edcacc93caaef3cf550b50196e46097c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -682,7 +682,7 @@ public class QSPanel extends LinearLayout implements Tunable {
 
     void setColumnRowLayout(boolean withMedia) {
         mTileLayout.setMinRows(withMedia ? 2 : 1);
-        mTileLayout.setMaxColumns(withMedia ? 2 : 4);
+        mTileLayout.setMaxColumns(withMedia ? 2 : 6);
         placeTileLayoutForScene(withMedia);
     }
 
@@ -843,6 +843,8 @@ public class QSPanel extends LinearLayout implements Tunable {
         int getNumVisibleTiles();
 
         default void setLogger(QSLogger qsLogger) { }
+
+        void updateSettings();
     }
 
     interface OnConfigurationChangedListener {
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -73,6 +73,7 @@ import com.android.systemui.plugins.qs.QSTileView
 import com.android.systemui.qs.logging.QSLogger
 import com.android.systemui.qs.tileimpl.QSIconViewImpl.QS_ANIM_LENGTH
 import com.android.systemui.res.R
+import com.android.internal.util.systemui.qs.QSLayoutUtils
 import java.util.Objects
 
 import com.android.internal.util.android.VibrationUtils
@@ -221,6 +222,10 @@ constructor(
     val areLongPressEffectPropertiesSet: Boolean
         get() = initialLongPressProperties != null && finalLongPressProperties != null
 
+    private var vertical = false
+    private var forceHideCheveron = true
+    private var labelHide = false
+
     init {
         val typedValue = TypedValue()
         if (!getContext().theme.resolveAttribute(R.attr.isQsTheme, typedValue, true)) {
@@ -230,8 +235,9 @@ constructor(
             )
         }
         setId(generateViewId())
-        orientation = LinearLayout.HORIZONTAL
-        gravity = Gravity.CENTER_VERTICAL or Gravity.START
+        vertical = QSLayoutUtils.getQSTileVerticalLayout(context)
+        labelHide = QSLayoutUtils.getQSTileLabelHide(context)
+        forceHideCheveron = vertical || labelHide
         importantForAccessibility = IMPORTANT_FOR_ACCESSIBILITY_YES
         clipChildren = false
         clipToPadding = false
@@ -239,15 +245,12 @@ constructor(
         background = createTileBackground()
         setColor(getBackgroundColorForState(QSTile.State.DEFAULT_STATE))
 
-        val padding = resources.getDimensionPixelSize(R.dimen.qs_tile_padding)
-        val startPadding = resources.getDimensionPixelSize(R.dimen.qs_tile_start_padding)
-        setPaddingRelative(startPadding, padding, padding, padding)
-
         val iconSize = resources.getDimensionPixelSize(R.dimen.qs_icon_size)
         addView(icon, LayoutParams(iconSize, iconSize))
 
         createAndAddLabels()
         createAndAddSideView()
+        updateResources()
     }
 
     override fun onConfigurationChanged(newConfig: Configuration?) {
@@ -280,11 +283,22 @@ constructor(
             width = iconSize
         }
 
+        if (vertical) {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
+        } else {
+            orientation = LinearLayout.HORIZONTAL
+            gravity = Gravity.CENTER_VERTICAL or Gravity.START
+        }
+
+        if (labelHide)
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
+
         val padding = resources.getDimensionPixelSize(R.dimen.qs_tile_padding)
-        val startPadding = resources.getDimensionPixelSize(R.dimen.qs_tile_start_padding)
+        val startPadding = if (vertical) padding else resources.getDimensionPixelSize(R.dimen.qs_tile_start_padding)
         setPaddingRelative(startPadding, padding, padding, padding)
 
-        val labelMargin = resources.getDimensionPixelSize(R.dimen.qs_label_container_margin)
+        val labelMargin = if (vertical) 0 else resources.getDimensionPixelSize(R.dimen.qs_label_container_margin)
         (labelContainer.layoutParams as MarginLayoutParams).apply { marginStart = labelMargin }
 
         (sideView.layoutParams as MarginLayoutParams).apply { marginStart = labelMargin }
@@ -306,7 +320,8 @@ constructor(
 
     private fun createAndAddLabels() {
         labelContainer =
-            LayoutInflater.from(context).inflate(R.layout.qs_tile_label, this, false)
+            LayoutInflater.from(context)
+                .inflate(if (vertical) R.layout.qs_tile_label_vertical else R.layout.qs_tile_label,this, false)
                 as IgnorableChildLinearLayout
         label = labelContainer.requireViewById(R.id.tile_label)
         secondaryLabel = labelContainer.requireViewById(R.id.app_label)
@@ -329,7 +344,8 @@ constructor(
             secondaryLabel.apply { typeface = Typeface.create("gsf-label-medium", Typeface.NORMAL) }
         }
 
-        addView(labelContainer)
+        if (!labelHide)
+            addView(labelContainer)
     }
 
     private fun createAndAddSideView() {
@@ -411,7 +427,8 @@ constructor(
         // is too short.
         val constrainedSquishiness = constrainSquishiness(squishinessFraction)
         bottom = top + (actualHeight * constrainedSquishiness).toInt()
-        scrollY = (actualHeight - height) / 2
+        scrollY = (actualHeight - height) / if (vertical) 7 else 2
+        label.alpha = if (!vertical) 1.0f else Math.pow(squishinessFraction.toDouble(), 7.0).toFloat()
         maybeUpdateLongPressEffectHeight(actualHeight.toFloat())
     }
 
@@ -858,7 +875,7 @@ constructor(
             customDrawableView.setImageDrawable(state.sideViewCustomDrawable)
             customDrawableView.visibility = VISIBLE
             chevronView.visibility = GONE
-        } else if (state !is AdapterState || state.forceExpandIcon) {
+        } else if ((state !is AdapterState || state.forceExpandIcon) && !forceHideCheveron) {
             customDrawableView.setImageDrawable(null)
             customDrawableView.visibility = GONE
             chevronView.visibility = VISIBLE
